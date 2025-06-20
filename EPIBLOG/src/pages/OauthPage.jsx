bdd6ca7dab1320767cef2a4fb48bebcbb7ab6d80
Simone Randine <randine95@icloud.com>
2025-06-20 20:05:31 +0200

    next

---
+++ EPIBLOG/src/pages/OauthPage.jsx
@@ -0,0 +1,27 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const OauthPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const token = params.get("token");
+    if (token) {
+      localStorage.setItem("token", token);
+      navigate("/dashboard");
+    } else {
+      navigate("/");
+    }
+  }, []);
+  return (
+    <>
+      <div>
+        <p>Login in corso</p>
+      </div>
+    </>
+  );
+};
+
+export default OauthPage;
